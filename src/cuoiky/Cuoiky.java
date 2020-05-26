f8991f6f9128fe2bb0598675e0bf7050b5a2f80a
TranPhuocLoi <phuocloi2203@gmail.com>
2020-05-27 02:44:22 +0700

    add import export file && custom train set data

---
+++ src/cuoiky/Cuoiky.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuoiky;
+
+/**
+ *
+ * @author nudo350
+ */
+public class Cuoiky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws Exception {
+        // TODO code application logic here
+//        MyKnowledgeModel new_model=new MyKnowledgeModel("C:\\Program Files\\Weka-3-8-4\\data\\qualitywine.arff");
+//        System.out.println(new_model);
+//        new_model.saveDataToCSV("D:\\ocela.csv");
+//            MyKnowledgeModel new_model = new MyKnowledgeModel("C:\\Program Files\\Weka-3-8-4\\data\\qualitywine.arff",null, null);
+//              new_model.trainset = new_model.divideTrainTest(new_model.dataset,20, false );
+//              new_model.testset = new_model.divideTrainTest(new_model.dataset,20, true );
+//              System.out.println(new_model);
+//              System.out.println(new_model.trainset.toSummaryString());
+//              System.out.println(new_model.testset.toSummaryString());
+
+             MyKnowledgeModel new_model = new MyKnowledgeModel("C:\\Program Files\\Weka-3-8-4\\data\\qualitywine.arff",null, null);
+              new_model.trainset = new_model.divideTrainTestR(new_model.dataset,80, false );
+              new_model.testset = new_model.divideTrainTestR(new_model.dataset,80, true );
+              System.out.println(new_model);
+              System.out.println(new_model.trainset.toSummaryString());
+              System.out.println(new_model.testset.toSummaryString());
+   }
+}
---
+++ src/cuoiky/MySVMModel.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cuoiky;
+
+/**
+ *
+ * @author nudo350
+ */
+public class MySVMModel {
+    
+}
